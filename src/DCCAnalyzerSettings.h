0e5ebddd89711e8a56077549b5c8f7ab9d537dd6
Mikael Ejberg Pedersen <83463793+ejbergdk@users.noreply.github.com>
2025-01-14 09:44:33 +0100

    Initial commit of DCCAnalyzer source files (#1)
    * Rename files
    
    * Add original implementation
    
    * clang-format changes

---
+++ src/DCCAnalyzerSettings.h
@@ -0,0 +1,27 @@
+#ifndef DCC_ANALYZER_SETTINGS
+#define DCC_ANALYZER_SETTINGS
+
+#include <AnalyzerSettings.h>
+#include <AnalyzerTypes.h>
+
+class DCCAnalyzerSettings : public AnalyzerSettings
+{
+  public:
+    DCCAnalyzerSettings();
+    virtual ~DCCAnalyzerSettings();
+
+    virtual bool SetSettingsFromInterfaces();
+    void UpdateInterfacesFromSettings();
+    virtual void LoadSettings(const char* settings);
+    virtual const char* SaveSettings();
+
+
+    Channel mInputChannel;
+    bool mStrictTiming;
+
+  protected:
+    AnalyzerSettingInterfaceChannel mInputChannelInterface;
+    AnalyzerSettingInterfaceBool mStrictTimingInterface;
+};
+
+#endif // DCC_ANALYZER_SETTINGS
