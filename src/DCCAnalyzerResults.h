dba683dd8d59cfd49b9b52e953ad8a09f8e44aeb
Mikael Ejberg Pedersen <83463793+ejbergdk@users.noreply.github.com>
2025-01-14 09:44:33 +0100

    Initial commit of DCCAnalyzer source files (#1)
    * Rename files
    
    * Add original implementation
    
    * clang-format changes

---
+++ src/DCCAnalyzerResults.h
@@ -0,0 +1,35 @@
+#ifndef DCC_ANALYZER_RESULTS
+#define DCC_ANALYZER_RESULTS
+
+#include <AnalyzerResults.h>
+
+class DCCAnalyzer;
+class DCCAnalyzerSettings;
+
+class DCCAnalyzerResults : public AnalyzerResults
+{
+  public:
+    DCCAnalyzerResults(DCCAnalyzer* analyzer, DCCAnalyzerSettings* settings);
+    virtual ~DCCAnalyzerResults();
+
+    virtual void GenerateBubbleText(U64 frame_index, Channel& channel, DisplayBase display_base);
+    virtual void GenerateExportFile(const char* file, DisplayBase display_base, U32 export_type_user_id);
+
+    virtual void GenerateFrameTabularText(U64 frame_index, DisplayBase display_base);
+    virtual void GeneratePacketTabularText(U64 packet_id, DisplayBase display_base);
+    virtual void GenerateTransactionTabularText(U64 transaction_id, DisplayBase display_base);
+
+    enum FrameType
+    {
+        FT_Preamble,
+        FT_Data,
+        FT_Checksum
+    };
+
+  protected: // functions
+  protected: // vars
+    DCCAnalyzerSettings* mSettings;
+    DCCAnalyzer* mAnalyzer;
+};
+
+#endif // DCC_ANALYZER_RESULTS
